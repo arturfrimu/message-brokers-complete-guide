392fd33791276324faffd56c0aca394f567ba03b
Artur Frimu <artur.frimu2@gmail.com>
2024-03-25 21:02:00 +0200

    Add kafka consumer module

---
+++ kafka-consumer-v1/src/main/java/com/arturfrimu/kafkaconsumerv1/KafkaConsumerV1Application.java
@@ -0,0 +1,13 @@
+package com.arturfrimu.kafkaconsumerv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConsumerV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaConsumerV1Application.class, args);
+    }
+
+}
---
+++ kafka-consumer-v1/src/test/java/com/arturfrimu/kafkaconsumerv1/KafkaConsumerV1ApplicationTests.java
@@ -0,0 +1,18 @@
+package com.arturfrimu.kafkaconsumerv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@RequiredArgsConstructor
+class KafkaConsumerV1ApplicationTests {
+
+    private final ApplicationContext applicationContext;
+
+    @Test
+    void contextLoads() {
+        assertThat(applicationContext).isNotNull();
+    }
+}
