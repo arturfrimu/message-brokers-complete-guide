eb8566ef3ce16480979d73fa7acb0593084999c4
Artur Frimu <artur.frimu2@gmail.com>
2024-03-26 22:40:32 +0200

    Clean pom.xml in rabbitmq module

--- rabbit-mq-v1/src/main/java/com/rabbitmq/RabbitMqV1Application.java
+++ rabbit-mq-v1/src/main/java/com/rabbitmq/RabbitMqV1Application.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RabbitMqV1Application {
-
     public static void main(String[] args) {
         SpringApplication.run(RabbitMqV1Application.class, args);
     }
-
 }
--- rabbit-mq-v1/src/test/java/com/rabbitmq/RabbitMqV1ApplicationTests.java
+++ rabbit-mq-v1/src/test/java/com/rabbitmq/RabbitMqV1ApplicationTests.java
@@ -1,13 +1,23 @@
 package com.rabbitmq;
 
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static lombok.AccessLevel.PRIVATE;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@RequiredArgsConstructor
+@FieldDefaults(level = PRIVATE, makeFinal = true)
 class RabbitMqV1ApplicationTests {
+
+    ApplicationContext applicationContext;
 
     @Test
     void contextLoads() {
+        assertThat(applicationContext).isNotNull();
     }
-
 }
