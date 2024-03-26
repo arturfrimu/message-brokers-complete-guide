8e25e912af753838071589f423d6ec8b4191f6f7
Artur Frimu <artur.frimu2@gmail.com>
2024-03-26 23:24:12 +0200

    Add CountDownLatch in MessageReceiver for testing

--- rabbit-mq-v1/src/main/java/com/rabbitmq/reciever/MessageReceiver.java
+++ rabbit-mq-v1/src/main/java/com/rabbitmq/reciever/MessageReceiver.java
@@ -4,12 +4,21 @@ import com.rabbitmq.configuration.RabbitMQConfig;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.CountDownLatch;
+
 @Component
 public class MessageReceiver {
+
+    private final CountDownLatch latch = new CountDownLatch(1);
 
     @RabbitListener(queues = RabbitMQConfig.queueName)
     public void receive(String in) {
         System.out.println(" [x] Received '" + in + "'");
+        latch.countDown();
+    }
+
+    public CountDownLatch getLatch() {
+        return latch;
     }
 }
 
