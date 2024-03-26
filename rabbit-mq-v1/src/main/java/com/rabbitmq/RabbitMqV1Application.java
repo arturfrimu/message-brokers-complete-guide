7cae80ccd54972510bba394d79041ad5554f411d
Artur Frimu <artur.frimu2@gmail.com>
2024-03-26 22:35:37 +0200

    Add rabbit mq module

---
+++ rabbit-mq-v1/src/main/java/com/rabbitmq/RabbitMqV1Application.java
@@ -0,0 +1,13 @@
+package com.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMqV1Application.class, args);
+    }
+
+}
